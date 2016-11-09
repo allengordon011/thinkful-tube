88c0e13360363ebc417b648a6b7f1461e6842695
Theresa Wilkin <theresawilkin@yahoo.com>
2016-11-09 15:46:45 -0500

    video plays

--- tube.js
+++ tube.js
@@ -1,39 +1,47 @@
-//document ready
-$(document).ready(function(){
-//get search input ==>
-//listener for submit button
-//store input in variable
-$('form').on('submit', function(e) {
-	 e.preventDefault();
-
-   var userValue = $('input').val();
-   searchYouTube(userValue, displayData);
-});
-
+//variables & state
 var ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
-//send JSon query==>
-//create query object
 
+
+//rendering
 function searchYouTube(userValue, callback){
 	var query = {
 		part: 'snippet',
 		key: 'AIzaSyAL6SU2FGzeWTOltP8Zg4KIauwmt9CgoV4',
 		q: userValue
 	}
-//get results of JSon query (getJSon)==>
 	$.getJSON(ENDPOINT, query, callback);
 }
 
-//post results of JSon query==>
 function displayData(data) {
 	var videos = data.items
 	videos.forEach(function(video) {
 		var imgUrl = video.snippet.thumbnails.default.url;
-		//link images to video
-		var imageLink = "https://www.youtube.com/watch?v=" + video.id.videoId;
-		var imgHtml = "<a href=" + imageLink + "> " + "<img src=" + imgUrl + ">";
+		var imgHtml = "<img src=" + imgUrl + " id=" + video.id.videoId + ">";
 		$(".js-results").append(imgHtml);
-		console.log(imgHtml);
 	})
 }
+
+
+//event listeners
+$(document).ready(function(){
+
+	$('form').on('submit', function(e) {
+		e.preventDefault();
+		var userValue = $('input').val();
+		searchYouTube(userValue, displayData);
+	});
+
+	$('input:text').click(function(){ 
+		$(this).val(''); 
+	});
+
+	$("div").on("click", "img", function(e) {
+		var id = $(this).attr("id");
+		var embed = '<iframe width="560" height="315" ' +
+		'src="https://www.youtube.com/embed/' + id + 
+		'" frameborder="0"' +
+		' allowfullscreen></iframe>'
+		$(".video").html(embed);
+	})
+
 });
