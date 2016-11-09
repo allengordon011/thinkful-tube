1cbf298824e394bd230429c89e7df19e30b895a1
trig0 <allengordon0@gmail.com>
2016-11-09 10:47:10 -0600

    pass to Theresa

---
+++ tube.js
@@ -0,0 +1,24 @@
+//document ready
+$(document).ready(function(){
+//get search input ==>
+//listener for submit button
+//store input in variable
+$('form').on('submit', function(e) {
+	
+});
+
+//send JSon query==>
+//create query object
+
+part: 'snippet'
+key: (your API key as a string)
+q: (your search term as a string)
+
+
+//get results of JSon query (getJSon)==>
+
+
+//post results of JSon query==>
+
+
+});
