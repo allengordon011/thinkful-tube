8cde7ef2383b7f25f634d996a564427bd18d7e46
Theresa Wilkin <theresawilkin@yahoo.com>
2016-11-09 12:41:12 -0500

    working version

--- tube.js
+++ tube.js
@@ -7,28 +7,31 @@ $('form').on('submit', function(e) {
 	 e.preventDefault();
 
    var userValue = $('input').val();
-	 console.log(userValue);
-
+   searchYouTube(userValue, displayData);
 });
 
+var ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
 //send JSon query==>
 //create query object
 
-function searchYouTube(userValue){
+function searchYouTube(userValue, callback){
 	var query = {
 		part: 'snippet',
 		key: 'AIzaSyAL6SU2FGzeWTOltP8Zg4KIauwmt9CgoV4',
 		q: userValue
 	}
-	$.getJSON()
+//get results of JSon query (getJSon)==>
+	$.getJSON(ENDPOINT, query, callback);
 }
 
-
-
-//get results of JSon query (getJSon)==>
-
-
 //post results of JSon query==>
-
+function displayData(data) {
+	var videos = data.items
+	videos.forEach(function(video) {
+		var imgUrl = video.snippet.thumbnails.default.url;
+		var imgHtml = "<img src=" + imgUrl + ">";
+		$(".js-results").append(imgHtml);
+	})
+}
 
 });
